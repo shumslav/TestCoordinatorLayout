f8e1cc435e57e46c0c71477165cb1f0501e18028
Vyacheslav Shumakov <slav.shumakov@yandex.ru>
2025-07-25 22:09:42 +0500

    save changes

--- app/src/main/java/com/example/testcoordinatorlayout/MainActivity.kt
+++ app/src/main/java/com/example/testcoordinatorlayout/MainActivity.kt
@@ -1,26 +1,76 @@
 package com.example.testcoordinatorlayout
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.testcoordinatorlayout.databinding.ActivityMainBinding
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    private val adapter by lazy {
+        MainAdapter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding.rvCatalog.adapter = adapter
+        binding.rvCatalog.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        adapter.submitList((0..100).toList())
+
+        setupMotionLayout()
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() = with(binding) {
+    }
+
+    private fun setupMotionLayout() = with(binding) {
+        main.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                if (p1 == R.id.all_expanded_started_set && p2 == R.id.without_stories_set) {
+                    if (stories.visibility == View.GONE) {
+                        main.transitionToState(R.id.without_stories_set)
+                    }
+                }
+
+                if (p1 == R.id.without_stories_set && p2 == R.id.all_expanded_started_set) {
+                    if (stories.visibility == View.GONE) {
+                        main.transitionToState(R.id.all_expanded_started_set)
+                    }
+                }
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                Log.d("TAG", "onTransitionChange")
+                Log.d("TAG", "p1 = $p1 p2 = $p2 p3= $p3")
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                Log.d("TAG", "onTransitionCompleted")
+                Log.d("TAG", "p1 = $p1")
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+                Log.d("TAG", "onTransitionTrigger")
+                Log.d("TAG", "p1 = $p1 p2 = $p2 p3= $p3")
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/example/testcoordinatorlayout/MainAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.testcoordinatorlayout
+
+import android.graphics.Color
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.setMargins
+import androidx.core.view.updateMarginsRelative
+import androidx.core.view.updatePadding
+import androidx.core.view.updatePaddingRelative
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class MainAdapter: ListAdapter<Int, MainAdapter.Holder>(diffUtil) {
+
+    inner class Holder(private val view: TextView): RecyclerView.ViewHolder(view) {
+
+        fun bind(value: Int) {
+            view.text = "Значение = $value"
+        }
+    }
+
+
+
+    companion object {
+        private val diffUtil = object: DiffUtil.ItemCallback<Int>() {
+            override fun areItemsTheSame(p0: Int, p1: Int): Boolean {
+                return p0 == p1
+            }
+
+            override fun areContentsTheSame(p0: Int, p1: Int): Boolean {
+                return p0 == p1
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): Holder {
+        return Holder(TextView(p0.context).apply {
+            layoutParams = RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.WRAP_CONTENT).apply {
+                updateMarginsRelative(start = 30, top = 60, end = 30)
+            }
+            updatePaddingRelative(start = 40, top = 40, bottom = 40)
+            setBackgroundColor(ContextCompat.getColor(p0.context, com.google.android.material.R.color.material_dynamic_primary10))
+            setTextColor(ContextCompat.getColor(p0.context, R.color.white))
+
+        })
+    }
+
+    override fun onBindViewHolder(p0: Holder, p1: Int) {
+        p0.bind(getItem(p1))
+    }
+}
